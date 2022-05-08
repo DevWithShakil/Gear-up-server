bd99731bf556a4d1680c3c1e6d6ebcc3026c1ae2
Shakil Khan <m.shakilkhan720@gmail.com>
2022-05-08 22:56:12 +0600

    deploy heroku

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+var jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { use } = require('express/lib/application');
+const res = require('express/lib/response');
 const port = process.env.PORT || 5000;
 
 // Use middleware
@@ -11,8 +13,20 @@ app.use(cors());
 app.use(express.json());
 
 
-//user: dbuser1
-//password: jfzdsXTDNZBRuTF8
+function verifyJwt(req, res, next) {
+    const authHeader = req.headers.authorization
+    if (!authHeader) {
+        return res.status(401).send({ message: "unauthorized" })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.DB_TOKEN, (error, decoded) => {
+        if (error) {
+            res.status(403).send({ message: "forbidden" })
+        }
+        req.decoded = decoded
+        next()
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lbaak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -64,6 +78,28 @@ async function run() {
             const query = req.body
             const result = await serviceCollection.insertOne(query)
             res.send(result)
+        })
+
+        // jwt token
+        app.post('/login', async (req, res) => {
+            const user = req.body
+            const accessToken = jwt.sign(user, process.env.DB_TOKEN, {
+                expiresIn: "1d"
+            })
+            res.send(accessToken)
+        })
+
+        // Mypost
+        app.get('/userpost', verifyJwt, async (req, res) => {
+            const email = req.query.email
+            console.log(email);
+            const decodedEmail = req.decoded.email
+            if (decodedEmail === email) {
+                const query = { email: email }
+                const cursor = serviceCollection.find(query)
+                const result = await cursor.toArray()
+                res.send(result)
+            }
         })
 
     }

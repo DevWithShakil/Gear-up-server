a6b7e6505894ddfec0db333e113ec7e82f01c7f9
Shakil Khan <m.shakilkhan720@gmail.com>
2022-05-08 17:29:28 +0600

    ap update

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-const express = require ('express');
-const cors = require ('cors');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const { use } = require('express/lib/application');
 const port = process.env.PORT || 5000;
 
 // Use middleware
-app.use (cors());
-app.use (express.json());
+app.use(cors());
+app.use(express.json());
 
 
 //user: dbuser1
 //password: jfzdsXTDNZBRuTF8
 
 
-const uri = "mongodb+srv://dbuser1:jfzdsXTDNZBRuTF8@cluster0.lbaak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.lbaak.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        const userCollection = client.db('bikeExpress').collection('user');
-        const user = {name: 'Shakil Khan', email: 'm.shakilkhan702@gmail.com'};
-        const result = await userCollection.insertOne(user);
-        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        const serviceCollection = client.db('bikeExpress').collection('service');
+
+        app.get('/service', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get('/service/:id', async (req, res) => {
+
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+
+        })
+        app.put('/service/:id', async (req, res) => {
+            const updateQuantity = req.body
+            console.log(updateQuantity);
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    quantity: updateQuantity.quantity
+                },
+            };
+            const result = await serviceCollection.updateOne(filter, updateDoc, options);
+            res.send(result)
+        })
+
     }
     finally {
 
@@ -31,11 +60,11 @@ async function run(){
     }
 }
 
-    run().catch(console.dir);
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send ('Running My Node Server')
+    res.send('Running My Node Server')
 });
 
 app.listen(port, () => {
